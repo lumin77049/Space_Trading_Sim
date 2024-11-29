665022cb0c09687939ccae913b03a6df9e46b0e2
lumin77 <lumin77049@gmail.com>
2024-11-29 18:02:54 +1100

    playing around with instances of structs

--- src/main.c
+++ src/main.c
@@ -2,19 +2,13 @@
 
 struct stock{
     float value;
-    int stock_num;
+    int stock_volume;
     char stock_tag[3]; // 3 character tag used as shorthand in any player-facing ui
 };
 
 int main(){
-
-    // defining a stock called iron
-    struct stock iron;
-    iron.value = 12.0f; // with a value of 12
-    iron.stock_num = 12567; // and with 12567 of them 'existing'
-
+    printf("You need to finish this project !!!");
     return 0;
 
-    // I'm going to quickly prototype this in python just so I have a base to
-    // work from.
+    // TODO: I *really* need to do something with this project lol
 }
